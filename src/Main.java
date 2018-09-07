a5fba4abc16b7d849fe384a75d9343dc3e00530d
Sarmad <sarmadsa2@gmail.com>
2018-09-07 12:47:34 +0200

    Add method test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Func funcitons =  new Func();
+        int[] numList = {1,2,3,4,5,6};
+
+        //Run methods
+        System.out.print(funcitons.sumList(numList));
     }
 }
